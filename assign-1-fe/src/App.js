4f0e147ec12df9762ff3ba6b0c10c232ef3e5602
dwyerm29 <103943292@student.swin.edu.au>
2023-08-25 12:13:20 +1000

    update anv

--- assign-1-fe/src/App.js
+++ assign-1-fe/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import SearchResults from "./pages/SearchResults";
+import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
 import { Container } from "@mui/material";
-import SearchResults from "./pages/SearchResults";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/searchresults" element={<SearchResults />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Container>
     </>
--- assign-1-fe/src/components/Navbar.js
+++ assign-1-fe/src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import { Input, Button } from "@material-tailwind/react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineUser,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 
 export default function Navbar() {
   const [nav, setNav] = useState(false);
+  const hide = () => setNav(false);
+  const show = () => setNav(true);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setNav(false)
+    );
+  }, []);
 
   const handleNav = () => {
     setNav(!nav);
@@ -12,8 +26,10 @@ export default function Navbar() {
   return (
     <div className="flex justify-between items-center h-24 max-width[1240px] m-auto px-4 text-white bg-gray-900">
       <Link to="/" className="text-3xl font-bold w-auto">
-        Site Name
+        GlowWave
       </Link>
+
+      {/* Handles Search Bar */}
       <div className="relative flex w-full gap-2 md:w-auto">
         <Input
           type="search"
@@ -27,6 +43,7 @@ export default function Navbar() {
           Search
         </Button>
       </div>
+      {/* Handles NavBar */}
       <ul className="hidden md:flex">
         <CustomeLink
           to="/searchresults"
@@ -40,7 +57,16 @@ export default function Navbar() {
         >
           About
         </CustomeLink>
+        <AiOutlineUser to="/login" size={20} className="" />
+        <CustomeLink
+          to="/login"
+          className="hover:bg-gray-300  active:bg-gray-900 p-4"
+        >
+          Login
+        </CustomeLink>
+        <AiOutlineShoppingCart to="/cart" />
       </ul>
+
       {/* Handles smaller screen */}
 
       <div onClick={handleNav} className="block md:hidden p-4">
@@ -54,21 +80,42 @@ export default function Navbar() {
             : "fixed left-[-100%]"
         }
       >
-        <Link to="/" className="w-full text-3xl font-bold">
-          Site Name
+        <Link
+          to="/"
+          className="w-full text-3xl font-bold"
+          onClick={handleNav}
+          onBlur={hide}
+          OnFocus={show}
+        >
+          GlowWave
         </Link>
         <ul className="uppercase p-4">
           <CustomeLink
             to="/searchresults"
+            onClick={handleNav}
+            onBlur={hide}
+            OnFocus={show}
             className="hover:bg-gray-300  active:bg-gray-900 border-b border-gray-600"
           >
             SearchResults
           </CustomeLink>
           <CustomeLink
             to="/about"
-            className="hover:bg-gray-300  active:bg-gray-900 border-b"
+            onClick={handleNav}
+            onBlur={hide}
+            OnFocus={show}
+            className="hover:bg-gray-300  active:bg-gray-900  border-b border-gray-600"
           >
             About
+          </CustomeLink>
+          <CustomeLink
+            to="/login"
+            onClick={handleNav}
+            onBlur={hide}
+            OnFocus={show}
+            className="hover:bg-gray-300  active:bg-gray-900 border-b"
+          >
+            Login
           </CustomeLink>
         </ul>
       </div>
