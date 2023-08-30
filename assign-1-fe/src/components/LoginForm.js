75de8789761ca980878caa887c5b9156a63d4d72
dwyerm29 <103943292@student.swin.edu.au>
2023-08-30 12:50:35 +1000

    update login

--- assign-1-fe/src/components/LoginForm.js
+++ assign-1-fe/src/components/LoginForm.js
@@ -3,23 +3,29 @@ import { Link } from "react-router-dom";
 
 export default function LoginForm() {
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-col justify-center px-4 py-10 lg:px-6">
       <h2 className="text-center text-3xl font-bold mt-20">Login Here</h2>
 
       <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6 bg-green-400 p-8 rounded-2xl ">
+        <form className="space-y-6 bg-green-400 p-6 rounded-2xl ">
           <div>
-            <label>Username/Email</label>
+            <label className="p-2">Username/Email</label>
             <input type="text" />
           </div>
           <div>
-            <label>Password</label>
+            <label className="p-2">Password</label>
             <input type="password" />
           </div>
           <div>
             <p>Forgot Password</p>
             <p>
-              Dont have an account? Register <Link to="/register">Here</Link>
+              Dont have an account? Register{" "}
+              <Link
+                to="/register"
+                className="underline hover:underline-offset-4"
+              >
+                Here
+              </Link>
             </p>
           </div>
           <Link to="/account">
