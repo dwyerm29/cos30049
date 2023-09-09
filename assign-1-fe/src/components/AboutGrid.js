1b40a497b34229b7e2344e594b79646ae7459c5a
Niko361 <alexnikolov684@gmail.com>
2023-09-09 23:52:54 +1000

    fix: added credits to aboutgrid.js

--- assign-1-fe/src/components/AboutGrid.js
+++ assign-1-fe/src/components/AboutGrid.js
@@ -1,5 +1,7 @@
 import React from "react";
 import homebg from "../images/homebg.jpg";
+import { Container } from "@mui/material";
+
 export default function AboutGrid() {
   return (
     <div className="w-full h-screen">
@@ -25,15 +27,62 @@ export default function AboutGrid() {
             Nikiolov(s100595852), Zac Ryder (s103643806)
           </p>
           <p className="text-l pt-2 sm:text-base">References:</p>
-          <p className="text-xs sm:text-base">
-            Photo by Alexander Ant:
-            https://www.pexels.com/photo/colorful-mix-of-neon-paints-swirling-on-black-surface-4585185/
-            Photo by Ryutaro Tsukata:
-            https://www.pexels.com/photo/moon-jellyfish-floating-in-water-5472598/
-            Photo by Maria Eduarda Loura Magalhães :
-            https://www.pexels.com/photo/artistic-woman-with-painted-face-in-uv-light-4856662/
-            https://unsplash.com/photos/7wBFsHWQDlk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
-          </p>
+          <ul className="text-xs sm:text-base">
+            <li>
+              Colorful mix of neon paints swirling on black surface by Alexander
+              Ant:
+              https://www.pexels.com/photo/colorful-mix-of-neon-paints-swirling-on-black-surface-4585185/
+            </li>
+            <li>
+              Photo by Ryutaro Tsukata:
+              https://www.pexels.com/photo/moon-jellyfish-floating-in-water-5472598/
+            </li>
+            <li>
+              Photo by Maria Eduarda Loura Magalhães:
+              https://www.pexels.com/photo/artistic-woman-with-painted-face-in-uv-light-4856662/
+            </li>
+            <li>
+              https://unsplash.com/photos/7wBFsHWQDlk?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+            </li>
+            <li>
+              A Pair of Purple Jellyfishes Floating Underwater by Twiggy Jia:
+              https://www.pexels.com/photo/a-pair-of-purple-jellyfishes-floating-underwater-8601366/
+            </li>
+            <li>
+              Pink and Black Illustration by Anni Roenkae:
+              https://www.pexels.com/photo/pink-and-black-illustration-3109816/
+            </li>
+            <li>
+              Pink and Multicolored Abstract Painting by Dids:
+              https://www.pexels.com/photo/pink-and-multicolored-abstract-painting-2911544/
+            </li>
+            <li>
+              Bright painted abstract background with flow effect and spots by
+              Damir Mijailovic:
+              https://www.pexels.com/photo/bright-painted-abstract-background-with-flow-effect-and-spots-3651579/
+            </li>
+            <li>
+              Faceless person standing with glowing neon threads on shoulders by
+              Marlene Leppänen:
+              https://www.pexels.com/photo/faceless-person-standing-with-glowing-neon-threads-on-shoulders-5621016/
+            </li>
+            <li>
+              Code Projected Over Woman by ThisIsEngineering:
+              https://www.pexels.com/photo/code-projected-over-woman-3861969/
+            </li>
+            <li>
+              Green Orchid Plant by Julia Sakelli:
+              https://www.pexels.com/photo/monochrome-photography-of-a-chimpanzee-605223/
+            </li>
+            <li>
+              Yellow Rose by Anthony:
+              https://www.pexels.com/photo/yellow-rose-133472/
+            </li>
+            <li>
+              White Rose by Pixabay Pixabay:
+              https://www.pexels.com/photo/white-rose-160916/
+            </li>
+          </ul>
         </div>
       </div>
     </div>
