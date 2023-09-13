e3f90ec82e71ec3d06761240d384007c067215f2
Niko361 <alexnikolov684@gmail.com>
2023-09-13 17:48:46 +1000

    feat: added more test queries and API calls

--- api_server/main.py
+++ api_server/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
 
 # CHECK README IF YOU GET AN ERROR HERE
 from db_config import db_config
@@ -22,128 +23,123 @@ app.add_middleware(
 )
 
 
-class Item(BaseModel):
-    name: str
-    description: str = None
-    price: float
-    tax: float = None
-
-
-# for section 3
-
-
-@app.get("/jsonData")
-async def funcTest():
-    jsonResult = {
-        "name": "Your name",
-        "Uni-year": 2,
-        "isStudent": True,
-        "hobbies": ["reading", "swimming"],
-    }
-
-    return jsonResult
-
-
+# return a list of users without their passwords
 @app.get("/users/")
 def get_users():
     try:
         # Establish a database connection
         connection = mysql.connector.connect(**db_config)
-
         # Create a cursor to execute SQL queries
         cursor = connection.cursor()
-
         # Define the SQL query to retrieve data (e.g., all users)
-        query = "SELECT * FROM users"
-
+        query = "SELECT user_id, first_name, last_name, email, wallet_id FROM users;"
         # Execute the SQL query
         cursor.execute(query)
-
         # Fetch all the rows
         result = cursor.fetchall()
-
         # Convert the result to a list of dictionaries
         users = [dict(zip(cursor.column_names, row)) for row in result]
-
         # Close the cursor and the database connection
         cursor.close()
         connection.close()
-
         return users
-
     except mysql.connector.Error as err:
         return {"error": f"Error: {err}"}
 
 
-@app.get("/all_assets/")
-def get_all_assets():
+# get a list of all assets. Optionally you may provide a list of categories to the query to match the assets using the following format: http://localhost:8000/assets/?category=1&category=2
+@app.get("/assets/")
+async def read_items(category: Annotated[list[int] | None, Query()] = None):
     try:
-        # Establish a database connection
         connection = mysql.connector.connect(**db_config)
-
-        # Create a cursor to execute SQL queries
         cursor = connection.cursor()
-
-        # Define the SQL query to retrieve data (e.g., all assets)
-        query = "SELECT * FROM assets"
-
-        # Execute the SQL query
+        query = "SELECT DISTINCT assets.token_id, item_name, item_description, image_url, image_thumbnail_url, image_resolution, selling_price, time_listed, filetype_name, license_name FROM assets LEFT JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id JOIN filetypes ON assets.image_filetype_id=filetypes.filetype_id JOIN licensetypes ON assets.license_type_id=licensetypes.license_type_id JOIN assetcategories ON assets.token_id=assetcategories.token_id"
+        if category != None:
+            if len(category) > 0:
+                query += " WHERE assetcategories.category_id='" + str(category[0]) + "'"
+            if len(category) > 1:
+                for i in range(1, len(category)):
+                    query += (
+                        " OR assetcategories.category_id='" + str(category[i]) + "'"
+                    )
         cursor.execute(query)
-
-        # Fetch all the rows
         result = cursor.fetchall()
-
-        # Convert the result to a list of dictionaries
         assets = [dict(zip(cursor.column_names, row)) for row in result]
-
-        # Close the cursor and the database connection
         cursor.close()
         connection.close()
-
         return assets
-
     except mysql.connector.Error as err:
         return {"error": f"Error: {err}"}
 
 
+# get information for a single asset, along with details like selling price if it exists
 @app.get("/asset/{token_id}")
 def get_assets(token_id: int):
     try:
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
-        query = "SELECT * FROM assets WHERE token_id =" + str(token_id)
+        query = (
+            "SELECT assets.token_id, item_name, item_description, image_url, image_thumbnail_url, image_resolution, selling_price, time_listed, filetype_name, license_name FROM assets LEFT JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id JOIN filetypes on assets.image_filetype_id=filetypes.filetype_id JOIN licensetypes on assets.license_type_id=licensetypes.license_type_id WHERE assets.token_id ="
+            + str(token_id)
+        )
         cursor.execute(query)
         result = cursor.fetchall()
         assets = [dict(zip(cursor.column_names, row)) for row in result]
         cursor.close()
         connection.close()
-
         return assets
-
     except mysql.connector.Error as err:
         return {"error": f"Error: {err}"}
 
 
+# get a list of all assets listed for sale along with other info
 @app.get("/listed_assets/")
 def get_listed_assets():
     try:
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
-
-        # NOTE THIS DOES NOT WORK
-        query = "SELECT * FROM assets INNER JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id INNER JOIN Transactions ON assets.token_id=(SELECT Transactions.token_id FROM Transactions WHERE Transactions.token_id=assets.token_id ORDER BY transaction_id DESC LIMIT 1)"
-
+        query = "SELECT assets.token_id, item_name, item_description, image_url, image_thumbnail_url, image_resolution, selling_price, time_listed, filetype_name, license_name FROM assets JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id JOIN filetypes on assets.image_filetype_id=filetypes.filetype_id JOIN licensetypes on assets.license_type_id=licensetypes.license_type_id"
         cursor.execute(query)
         result = cursor.fetchall()
-
         print(result)
-
         assets = [dict(zip(cursor.column_names, row)) for row in result]
         cursor.close()
         connection.close()
-
         return assets
+    except mysql.connector.Error as err:
+        return {"error": f"Error: {err}"}
 
+
+# return a list of transactions that a particular user has been involved in
+@app.get("/user_transactions/")
+def get_user_transactions():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        query = "SELECT * FROM transactions WHERE seller_id = '1' OR buyer_id = '1'"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        assets = [dict(zip(cursor.column_names, row)) for row in result]
+        cursor.close()
+        connection.close()
+        return assets
+    except mysql.connector.Error as err:
+        return {"error": f"Error: {err}"}
+
+
+# get a list of all asset categories along with their descriptions
+@app.get("/asset_categories/")
+def get_asset_categories():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        query = "SELECT * FROM AssetCategoryDescriptions"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        assets = [dict(zip(cursor.column_names, row)) for row in result]
+        cursor.close()
+        connection.close()
+        return assets
     except mysql.connector.Error as err:
         return {"error": f"Error: {err}"}
 
@@ -151,16 +147,6 @@ def get_listed_assets():
 @app.get("/")
 async def funcTest1():
     return "Hello, this is fastAPI data"
-
-
-@app.get("/getAboutData")
-async def funcTest2():
-    return "Hello, this is about us data"
-
-
-@app.get("/getHomeData")
-async def funcTest3():
-    return "Hello, this is home data"
 
 
 @app.get("/jsonData")
@@ -177,6 +163,13 @@ async def funcTest():
 @app.get("/student/{student_id}")
 async def getStudentId(student_id: int):
     return {"student_id": student_id}
+
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
 
 
 @app.post("/items/", response_model=Item)
--- database/testqueries.sql
+++ database/testqueries.sql
@@ -1,5 +1,45 @@
--- get a list of all assets for sale along with their current owner
+-- get a list of all assets for sale along with their current owner (DOES NOT WORK)
 SELECT * FROM assets 
     JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id 
     JOIN transactions ON assets.token_id=(SELECT transactions.token_id FROM transactions WHERE transactions.token_id=assets.token_id ORDER BY transaction_id DESC LIMIT 1)
     AND assets.token_id=transactions.token_id
+
+-- return a list of all assets along with information like selling price, file type and license type
+SELECT assets.token_id, item_name, item_description, image_url, image_thumbnail_url, image_resolution, selling_price, time_listed, filetype_name, license_name 
+FROM assets LEFT JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id 
+JOIN filetypes ON assets.image_filetype_id=filetypes.filetype_id 
+JOIN licensetypes ON assets.license_type_id=licensetypes.license_type_id
+
+-- get a list of all assets listed for sale along with other info
+SELECT assets.token_id, item_name, item_description, image_url, image_thumbnail_url, image_resolution, selling_price, time_listed, filetype_name, license_name 
+    FROM assets 
+    JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id 
+    JOIN filetypes ON assets.image_filetype_id=filetypes.filetype_id
+    JOIN licensetypes ON assets.license_type_id=licensetypes.license_type_id
+
+
+-- get information for a single asset, along with details like selling price if it exists
+SELECT assets.token_id, item_name, item_description, image_url, image_thumbnail_url, image_resolution, selling_price, time_listed, filetype_name, license_name 
+    FROM assets 
+    LEFT JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id 
+    JOIN filetypes ON assets.image_filetype_id=filetypes.filetype_id
+    JOIN licensetypes ON assets.license_type_id=licensetypes.license_type_id
+    WHERE assets.token_id='2623426'
+
+-- return a list of users without their passwords
+SELECT user_id, first_name, last_name, email, wallet_id FROM users;
+
+-- return a list of transactions that a particular user has been involved in
+SELECT * FROM transactions WHERE seller_id = '1' OR buyer_id = '1'
+
+-- get a list of all asset categories along with their descriptions
+SELECT * FROM AssetCategoryDescriptions
+
+--get a list of assets that match one of any category IDs
+SELECT DISTINCT assets.token_id, item_name, item_description, image_url, image_thumbnail_url, image_resolution, selling_price, time_listed, filetype_name, license_name 
+    FROM assets 
+    LEFT JOIN assetslistedforsale ON assets.token_id=assetslistedforsale.token_id 
+    JOIN filetypes ON assets.image_filetype_id=filetypes.filetype_id
+    JOIN licensetypes ON assets.license_type_id=licensetypes.license_type_id
+    JOIN assetcategories ON assets.token_id=assetcategories.token_id
+    WHERE assetcategories.category_id='1' OR assetcategories.category_id='2'
