a556a8fe2b011800fc8d6aea20cdf2af3c557e33
Alexander Nikolov <alexnikolov684@gmail.com>
2023-10-18 03:15:29 +1100

    feat: progress on transactions DB smart contract

--- api_server/SimpleStorage.sol
+++ api_server/SimpleStorage.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
 contract SimpleStorage {
     uint256 favoriteNumber;
@@ -22,12 +23,41 @@ contract SimpleStorage {
         return favoriteNumber;
     }
 
-    function retrieve() public view returns (uint256) {
+    function retrieveFavouriteNumber() public view returns (uint256) {
         return favoriteNumber;
     }
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
         nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    struct Transaction {
+        uint256 transaction_id;
+        uint256 token_id;
+        uint256 seller_id;
+        uint256 buyer_id;
+        uint256 sale_time;
+        uint256 sale_price;
+    }
+
+
+    Transaction[] public transactions;
+    mapping(uint256 => uint256) public tokenIdToOwnerId;
+
+    function addTransaction(uint256 _token_id, uint256 _seller_id, uint256 _buyer_id, uint256 _sale_time, uint256 _sale_price) public {
+        uint256 newTransactionID = transactions.length + 1;
+        favoriteNumber = newTransactionID;
+        transactions.push(Transaction({transaction_id: newTransactionID, token_id: _token_id, seller_id : _seller_id, buyer_id: _buyer_id, sale_time: _sale_time, sale_price: _sale_price}));
+
+        //tokenIdToOwnerId[_token_id] = _seller_id;
+    }
+
+    function getAllTransactions() public view returns (Transaction[] memory) {
+
+
+        //Transaction[] memory return_transactions = new Transaction[](1);
+
+        return transactions;
     }
 }
