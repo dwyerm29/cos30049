10428cb4e4441c418637d677421415fd78cb9fa1
Niko361 <alexnikolov684@gmail.com>
2023-10-19 10:14:58 +1100

    fix: removed blockchain_config.py

--- api_server/blockchain_config.py
+++
@@ -1,4 +0,0 @@
-blockchain_config = {
-    "account_address": "0x33ca64adc368A91b44B87BF434Ced08bA3C4174e",
-    "account_private_key": "0xd6b2c7c0271fcc27a4ba406644b80f66d083d8803622e60c40445b3adc72e0bb"
-}
