abeab7a36542174b3c76983f32cea1aa23b63dca
dwyerm29 <103943292@student.swin.edu.au>
2023-10-18 14:26:33 +1100

    Create blockchain_config.py

---
+++ api_server/blockchain_config.py
@@ -0,0 +1,4 @@
+blockchain_config = {
+    "account_address": "0x0Fbd2F2cbc2FA85E9066030fEDAe171b132343B5",
+    "account_private_key": "0xa29dc330c00c064687dd228df566fd6ecc878229adfcd6bc8256dd3241e8ac56"
+}
