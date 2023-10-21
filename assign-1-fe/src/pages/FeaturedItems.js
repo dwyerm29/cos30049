cf473d4d760334c3b6a57b3b8793ff21ca3ee2c6
Alexander Nikolov <alexnikolov684@gmail.com>
2023-10-22 00:52:46 +1100

    feat: added categories and sort fo featured page

--- assign-1-fe/src/pages/FeaturedItems.js
+++ assign-1-fe/src/pages/FeaturedItems.js
@@ -1,27 +1,214 @@
 import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Box,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Grid,
+  OutlinedInput,
+  Checkbox,
+  ListItemText,
+  Alert,
+} from "@mui/material";
 
 import GalleryGrid from "../components/GalleryGrid";
-import { Container } from "@mui/material";
 
 import axios from "axios";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 export const FeaturedItems = () => {
   const [featuredItems, setFeaturedItems] = useState([]);
 
+  const [sortType, setSort] = useState(0);
+
+  const [categoryFilters, setCategoryFilters] = useState([]);
+
+  const handleSortChange = (event) => {
+    setSort(event.target.value);
+    switch (event.target.value) {
+      case 0:
+        setFeaturedItems(
+          featuredItems.sort((a, b) => {
+            const nameA = a.item_name.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.item_name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            return 0;
+          })
+        );
+        console.log("sort by Name");
+        break;
+      case 1:
+        setFeaturedItems(
+          featuredItems.sort((a, b) => a.selling_price - b.selling_price)
+        );
+        console.log("sort by price (lowest)");
+        break;
+      case 2:
+        setFeaturedItems(
+          featuredItems.sort((a, b) => b.selling_price - a.selling_price)
+        );
+        console.log("sort by price (highest)");
+        break;
+      case 3:
+        setFeaturedItems(
+          featuredItems.sort((a, b) => {
+            const timedateA = a.time_listed;
+            const timedateB = b.time_listed;
+            if (timedateA > timedateB) {
+              return -1;
+            }
+            if (timedateA < timedateB) {
+              return 1;
+            }
+            return 0;
+          })
+        );
+        console.log("sort by recently added");
+        break;
+      case 4:
+        setFeaturedItems(
+          featuredItems.sort((a, b) => {
+            const timedateA = a.time_listed;
+            const timedateB = b.time_listed;
+            if (timedateA < timedateB) {
+              return -1;
+            }
+            if (timedateA > timedateB) {
+              return 1;
+            }
+            return 0;
+          })
+        );
+        console.log("sort by oldest");
+        break;
+      default:
+    }
+  };
+
+  const [filterVal, setFilter] = useState("");
+
+  const handleFilterChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    if (!value.includes("Featured")) value.push("Featured");
+    setFilter(value);
+  };
+
+  useEffect(() => {
+    var categoryQuery = "&category=Featured";
+    for (const cat of filterVal) {
+      categoryQuery += `&category=${cat}`;
+    }
+    axios
+      .get(`http://127.0.0.1:8000/assets/search/?query=${categoryQuery}`)
+      .then((response) => {
+        setFeaturedItems(
+          response.data.sort((a, b) => {
+            const nameA = a.item_name.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.item_name.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            return 0;
+          })
+        );
+      })
+      .catch((error) => {
+        console.error("error here: ", error);
+      });
+  }, [filterVal]);
+
   useEffect(() => {
     axios
-      .get(`http://127.0.0.1:8000/featured_assets/`)
+      .get(`http://127.0.0.1:8000/asset_categories`)
       .then((response) => {
-        setFeaturedItems(response.data);
+        var categories = [];
+        response.data.map((category) => {
+          categories.push(category.category_name);
+        });
+        setCategoryFilters(categories);
+        setFilter(["Featured"]);
       })
       .catch((error) => {
         console.error("error here: ", error);
       });
   }, []);
+  const noResult = featuredItems.length === 0;
 
   return (
-    <Container>
-      <GalleryGrid items={featuredItems} />
-    </Container>
+    <div>
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between items-center max-width[1240px] m-auto pt-4 ">
+          <Grid container justifyContent="flex-end">
+            <Box sx={{ minWidth: 200, mx: 2 }}>
+              <FormControl fullWidth>
+                <InputLabel id="filter-categories">Categories</InputLabel>
+                <Select
+                  labelId="filter-categories"
+                  id="filter-categories"
+                  multiple
+                  value={filterVal || []}
+                  onChange={handleFilterChange}
+                  input={<OutlinedInput label="Category" />}
+                  renderValue={(selected) => selected.join(", ")}
+                  MenuProps={MenuProps}
+                >
+                  {categoryFilters.map((filterName) => (
+                    <MenuItem key={filterName} value={filterName}>
+                      <Checkbox checked={filterVal.indexOf(filterName) > -1} />
+                      <ListItemText primary={filterName} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            <Box sx={{ minWidth: 200 }}>
+              <FormControl fullWidth>
+                <InputLabel id="sort-categories">Sort</InputLabel>
+                <Select
+                  labelId="sort-categories"
+                  id="sort-categories"
+                  value={sortType}
+                  label="sort-categories"
+                  onChange={handleSortChange}
+                >
+                  <MenuItem value={0}>Name</MenuItem>
+                  <MenuItem value={1}>Price Low to High</MenuItem>
+                  <MenuItem value={2}>Price High to Low</MenuItem>
+                  <MenuItem value={3}>Recently Added</MenuItem>
+                  <MenuItem value={4}>Oldest Added</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Grid>
+        </div>
+        {noResult ? (
+          <Alert severity="warning">No items found. Please try again!</Alert>
+        ) : (
+          <GalleryGrid items={featuredItems} />
+        )}
+      </Container>
+    </div>
   );
 };
